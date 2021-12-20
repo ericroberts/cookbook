16223997917c3074c6b83781ee9241d76b4b930f
Eric Roberts <ericroberts@gmail.com>
2021-12-20 15:28:54 -0500

    generate css when building

--- lib/builder.rb
+++ lib/builder.rb
@@ -4,6 +4,9 @@ require_relative "recipe"
 
 class Builder
   def self.build
+    puts "generating CSS"
+    puts system("npx tailwindcss -i ./src/input.css -o ./dist/styles.css")
+
     layout = Slim::Template.new("src/templates/layout.slim")
 
     recipes = Dir.glob("src/recipes/*.yml").map do |f|
@@ -16,9 +19,12 @@ class Builder
       layout.render { Slim::Template.new("src/templates/recipes.slim").render(recipes) },
     )
 
+    puts "Index written"
+
     recipes.each do |recipe|
       FileUtils.mkdir_p "dist/recipes/#{recipe.slug}"
       File.write("dist/recipes/#{recipe.slug}/index.html", layout.render { recipe.render })
+      puts "#{recipe.slug} written"
     end
   end
 end
