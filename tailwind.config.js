b9e2127ad5cf658783666d7a60a219155bde857d
Eric Roberts <ericroberts@gmail.com>
2021-12-22 13:53:16 -0500

    Hover styles for index page links

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,11 @@
 module.exports = {
   content: ["./dist/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      boxShadow: {
+        'link': 'inset 0 -.3em 0 0 #50fa7b',
+      },
+    },
   },
   plugins: [],
 }
