35abaabdde4c462c7a2f99a837bf80bcf30f7801
Eric Roberts <ericroberts@gmail.com>
2021-12-29 20:34:24 -0500

    dark mode

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,22 @@ module.exports = {
       boxShadow: {
         'link': 'inset 0 -.3em 0 0 #50fa7b',
       },
+      colors: {
+        'light': '#fafafa',
+        'light-foreground': '#3e4047',
+        'dracula-background': '#282a36',
+        'dracula-code-bg': '#1b1d29',
+        'dracula-foreground': '#f8f8f2',
+        'dracula-current-line': '#44475a',
+        'dracula-comment': '#6272a4',
+        'dracula-cyan': '#8be9fd',
+        'dracula-green': '#50fa7b',
+        'dracula-orange': '#ffb86c',
+        'dracula-pink': '#ff79c6',
+        'dracula-purple': '#bd93f9',
+        'dracula-red': '#ff5555',
+        'dracula-yellow': '#f1fa8c'
+      },
     },
   },
   plugins: [],
